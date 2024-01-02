1335dbd6fb111262dbcff77146b770b50eb44e59
HenryToff <144433130+HenryToff@users.noreply.github.com>
2024-01-02 10:54:24 -0100

    Add files via upload

--- script.js
+++ script.js
@@ -18,6 +18,22 @@ window.onscroll =() =>{
     }
 }
 
+     var data = window.location.search;
+     var params = new URLSearchParams(data);
+
+     //console.log(params);
+
+     var tittle = params.get("tittle");
+     var page = params.get("page");
+
+     var tittle = tittle;
+         page = page;
+
+     document.getElementById("tittle").innerHTML = tittle;
+     document.getElementById("page").innerHTML = page;
+
+ 
+
 
 
 
