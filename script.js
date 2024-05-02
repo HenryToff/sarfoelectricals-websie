d2e83047959847ab7e74fb4f823fa05ebbbf7c53
HenryToff <144433130+HenryToff@users.noreply.github.com>
2024-05-02 07:19:08 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -18,56 +18,16 @@ window.onscroll =() =>{
     }
 }
 
-     var data = window.location.search;
-     var params = new URLSearchParams(data);
+    // var data = window.location.search;
+     //var params = new URLSearchParams(data);
 
      //console.log(params);
 
-     var tittle = params.get("tittle");
-     var page = params.get("page");
+     //var tittle = params.get("tittle");
+     //var page = params.get("page");
 
-     var tittle = tittle;
-         page = page;
+     //var tittle = tittle;
+        // page = page;
 
-     document.getElementById("tittle").innerHTML = tittle;
-     document.getElementById("page").innerHTML = page;
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+     //document.getElementById("tittle").innerHTML = tittle;
+     //document.getElementById("page").innerHTML = page;
